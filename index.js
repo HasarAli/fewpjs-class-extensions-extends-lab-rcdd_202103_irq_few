2bae1a47b3068ecb8edd59ed041d7f3fc1fd0e58
Hasar Ali <hasar.ali@protonmail.com>
2021-06-06 23:57:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+
+class Polygon {
+  constructor(sides) {
+    this.sides = sides
+  }
+  
+  get countSides() {
+    return this.sides.length
+  }
+}
+
+const res = new Polygon([1,2,3]).countSides
+console.log(res)
