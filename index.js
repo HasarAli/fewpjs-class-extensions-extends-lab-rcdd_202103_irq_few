be9a61a4e93359879cc53a8d35675a8fdeb08cf6
Hasar Ali <hasar.ali@protonmail.com>
2021-06-06 23:58:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ class Polygon {
   get countSides() {
     return this.sides.length
   }
+  
+  get perimeter() {
+    return this.sides.reduce(perimeter, (side => perimeter += side))
+  }
 }
 
 const res = new Polygon([1,2,3]).countSides
